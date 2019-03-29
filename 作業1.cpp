dc1e3b1decb0a445ce445d4d7601dfc13854f71f
TaiWeiHo <48812548+TaiWeiHo@users.noreply.github.com>
2019-03-29 11:10:20 +0800

    Add files via upload

---
+++ 作業1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	cout<< "hello world 3A713243";
+	system("PAUSE");
+		return 0;
+}
